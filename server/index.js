97e8afb840385438bf9c9ef9742d879abd4037e3
MOHAMED AMINE STASAID <aminestasaid33@gmail.com>
2025-03-01 21:32:03 +0100

    fixed vercel

--- server/index.js
+++ server/index.js
@@ -16,10 +16,23 @@ import addressRouter from './route/address.route.js'
 import orderRouter from './route/order.route.js'
 
 const app = express()
-app.use(cors({
-    credentials : true,
-    origin : process.env.FRONTEND_URL
-}))
+const allowedOrigins = [
+    "https://www.lamsaemballage.com/",
+    "https://lamsa-emballage.vercel.app/",
+  ];
+app.use(
+    cors({
+      origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
+      credentials: true,
+    })
+  );
+  
 app.use(express.json())
 app.use(cookieParser())
 app.use(morgan())
